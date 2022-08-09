c1b403419e3ffe6c3f9ed0b1dc43b1958bbda6e0
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-08-10 01:18:42 +0900

    (wip) Partially fix problem `var type_value_test4:=Animation.TrackType.TYPE_BLEND_SHAPE` results in ``` SCRIPT ERROR: Parse Error: Could not infer the type of the variable "type_value_test4" because the initial value is a variant. Use explicit "Variant" type if this is intended. ```

--- modules/gdscript/gdscript_analyzer.cpp
+++ modules/gdscript/gdscript_analyzer.cpp
@@ -499,7 +499,11 @@ GDScriptParser::DataType GDScriptAnalyzer::resolve_datatype(GDScriptParser::Type
 			return GDScriptParser::DataType();
 		}
 		result = ref->get_parser()->head->get_datatype();
+#ifdef DEBUG_ENABLED
 	} else if (ClassDB::has_enum(parser->current_class->base_type.native_type, first)) {
+#else
+	} else if (ClassDB::has_enum(parser->current_class->base_type.native_type, first) || ClassDB::classes.has(parser->current_class->base_type.native_type)) {
+#endif
 		// Native enum in current class.
 		result = make_native_enum_type(parser->current_class->base_type.native_type, first);
 	} else {
@@ -582,6 +586,7 @@ GDScriptParser::DataType GDScriptAnalyzer::resolve_datatype(GDScriptParser::Type
 			}
 		} else if (result.kind == GDScriptParser::DataType::NATIVE) {
 			// Only enums allowed for native.
+#ifdef DEBUG_ENABLED
 			if (ClassDB::has_enum(result.native_type, p_type->type_chain[1]->name)) {
 				if (p_type->type_chain.size() > 2) {
 					push_error(R"(Enums cannot contain nested types.)", p_type->type_chain[2]);
@@ -591,6 +596,10 @@ GDScriptParser::DataType GDScriptAnalyzer::resolve_datatype(GDScriptParser::Type
 			} else {
 				push_error(vformat(R"(Could not find nested enum type "%s" in class "%s".)", p_type->type_chain[1]->name, result.native_type), p_type->type_chain[1]);
 			}
+#else
+			result.kind = GDScriptParser::DataType::ENUM;
+			result.builtin_type = Variant::Type::INT;
+#endif
 		} else {
 			push_error(vformat(R"(Could not find nested type "%s" under base "%s".)", p_type->type_chain[1]->name, result.to_string()), p_type->type_chain[1]);
 			result.kind = GDScriptParser::DataType::VARIANT; // Leave Variant anyway so future type check don't use an unresolved type.
